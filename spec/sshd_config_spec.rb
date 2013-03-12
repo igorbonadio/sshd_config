a88887767132cbf62ae27d92cb26d5916c3a2f22
Ígor Bonadio <igorbonadio@gmail.com>
2013-03-12 17:31:29 -0300

    some specs

--- spec/sshd_config_spec.rb
+++ spec/sshd_config_spec.rb
@@ -1,7 +1,11 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "SshdConfig" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
-  end
+  it "should open a sshd_config file"
+  it "should read a sshd_config file"
+  it "should get parameters from sshd_config file"
+  it "should set parameters from sshd_config file"
+  it "should write a sshd_config file"
+  it "should keep original comments"
+  it "should write new comments"
 end
