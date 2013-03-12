db08878de2c160f111e709852bc2cc2c212492e5
Ígor Bonadio <igorbonadio@gmail.com>
2013-03-12 18:01:11 -0300

    SshdConfig opens a sshd_config file

--- lib/gritano-sshconfig/sshd_config.rb
+++ lib/gritano-sshconfig/sshd_config.rb
@@ -1,4 +1,8 @@
 module Gritano
   class SshdConfig
+    def initialize(filename)
+      @filename = filename
+      @file = File.open(@filename, "r")
+    end
   end
 end
--- spec/sshd_config_spec.rb
+++ spec/sshd_config_spec.rb
@@ -1,11 +1,17 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "SshdConfig" do
-  it "should open a sshd_config file"
-  it "should read a sshd_config file"
-  it "should get parameters from sshd_config file"
-  it "should set parameters from sshd_config file"
-  it "should write a sshd_config file"
-  it "should keep original comments"
-  it "should write new comments"
+module Gritano
+  describe "SshdConfig" do
+    it "should open a sshd_config file" do
+      File.should_receive(:open).with(File.join(File.dirname(__FILE__), 'data', 'sshd_config'), "r")
+      SshdConfig.new(File.join(File.dirname(__FILE__), 'data', 'sshd_config'))
+    end
+    
+    it "should read a sshd_config file"
+    it "should get parameters from sshd_config file"
+    it "should set parameters from sshd_config file"
+    it "should write a sshd_config file"
+    it "should keep original comments"
+    it "should write new comments"
+  end
 end
